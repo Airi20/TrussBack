092c96c3c9daeb07890705ef6c25fe13b9ca8c8e
Airi Ishikawa <aiai9004432221@gmail.com>
2025-06-24 00:50:11 +0900

    Update WebConfig.java

--- src/main/java/com/airi/trussforce/demo/config/WebConfig.java
+++ src/main/java/com/airi/trussforce/demo/config/WebConfig.java
@@ -6,12 +6,18 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3002")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")   // APIのパス全般を許可
+                        .allowedOrigins("https://trussfront.onrender.com")  // フロントのドメインだけOKにする
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
-
