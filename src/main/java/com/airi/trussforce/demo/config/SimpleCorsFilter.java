c0a9d28193a18d8174ea8182717bfb5639bf901a
Airi Ishikawa <aiai9004432221@gmail.com>
2025-06-24 11:59:50 +0900

    Add files via upload

---
+++ src/main/java/com/airi/trussforce/demo/config/SimpleCorsFilter.java
@@ -0,0 +1,32 @@
+package com.airi.trussforce.demo.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class SimpleCorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", "https://trussfront.onrender.com");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return; // OPTIONSはここで終わらせる
+        }
+
+        chain.doFilter(req, res);
+    }
+}
